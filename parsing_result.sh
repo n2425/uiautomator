f242acc06867d73dbc8ecac0cfe099ab6849c807
hm.kim <hm.kim@the-aio.com>
2015-11-16 17:14:23 -0800

    parser bug fix

--- parsing_result.sh
+++ parsing_result.sh
@@ -2,6 +2,7 @@
 INPUT_FILE_NAME=
 OUTPUT_FILE_NAME=
 AP_NAME=
+WHOLE_DATA=./data.tmp
 SQLITE3="$HOME/Android/Sdk/platform-tools/sqlite3"
 
 function usage()
@@ -16,13 +17,14 @@ function parse_result()
     do
         result=`${SQLITE3} $INPUT_FILE_NAME 'select * from history' | tail -10 | sed -n ${loop_count}p`;
         result=`echo $result | awk -F'|' '{print $9,$10,$12,$14}'`
-        whole_data=`echo -n $result `
+        echo -n $result >> ${WHOLE_DATA}
+        echo -n " " >> ${WHOLE_DATA}
     done 
 
-    result_seq_read=`echo ${whole_data} | awk 'BEGIN {OFS = ","} {print $1,$5,$9,$13,$17,$21,$25,$29,$33,$37}'`;
-    result_seq_write=`echo ${whole_data} | awk 'BEGIN {OFS = ","} {print $2,$6,$10,$14,$18,$22,$26,$30,$34,$38}'`;
-    result_rand_read=`echo ${whole_data} | awk 'BEGIN {OFS = ","} {print $3,$7,$11,$15,$19,$23,$27,$31,$35,$39}'`;
-    result_rand_write=`echo ${whole_data} | awk 'BEGIN {OFS = ","} {print $4,$8,$12,$16,$20,$24,$28,$32,$36,$40}'`;
+    result_seq_read=`cat ${WHOLE_DATA} | awk 'BEGIN {OFS = ","} {print $1,$5,$9,$13,$17,$21,$25,$29,$33,$37}'`;
+    result_seq_write=`cat ${WHOLE_DATA} | awk 'BEGIN {OFS = ","} {print $2,$6,$10,$14,$18,$22,$26,$30,$34,$38}'`;
+    result_rand_read=`cat ${WHOLE_DATA} | awk 'BEGIN {OFS = ","} {print $3,$7,$11,$15,$19,$23,$27,$31,$35,$39}'`;
+    result_rand_write=`cat ${WHOLE_DATA} | awk 'BEGIN {OFS = ","} {print $4,$8,$12,$16,$20,$24,$28,$32,$36,$40}'`;
 
     echo "-${AP_NAME}-" >> $OUTPUT_FILE_NAME;
     echo $result_seq_read >> $OUTPUT_FILE_NAME;
@@ -32,7 +34,7 @@ function parse_result()
     echo "" >> $OUTPUT_FILE_NAME;
 }
 
-if [ $# -ne 3 ];then
+if [ $# -ne 4 ];then
 	usage
 fi
 
@@ -42,6 +44,8 @@ OUTPUT_FILE_NAME=$3
 LOOP_COUNT=$4
 
 parse_result;
+
+rm $WHOLE_DATA;
 
 
 
